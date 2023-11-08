2ea75f00b11af2103825bfd9575e6775d126dee4
Richard <richard.lyzx@gmail.com>
2023-11-08 17:33:19 -0500

    increased dataset

--- test.py
+++ test.py
@@ -1,26 +1,76 @@
 import spacy
-from spacy.tokens import Doc
+from spacy.training import Example
+import random
+#    ("Check the performance of CAD treasury bonds today", {"entities": [(25, 28, "currency"), (41, 46, "time_interval"), (29,44, "instrument")]}),
+#     ("What was the closing value of USD treasury bonds yesterday?", {"entities": [(30, 33, "currency"), (49, 59, "time_interval"), (34,49, "instrument")]}),
+#     ("Get me the summary for CAD treasury bonds from six months ago", {"entities": [(18, 21, "currency"), (38, 52, "time_interval"), (22,37, "instrument")]}),
+#     ("Provide a breakdown of USD treasury bonds for the last month", {"entities": [(23, 26, "currency"), (42, 52, "time_interval"), (27,42, "instrument")]}),
+#     ("I want to review CAD treasury bonds over the past year", {"entities": [(17, 20, "currency"), (44, 52, "time_interval"), (21,36, "instrument")]}),
+#     ("USD treasury bond index figures from a year ago, please", {"entities": [(0, 3, "currency"), (39, 47, "time_interval"), (4,24, "instrument")]}),
+#     ("Show the trend for CAD treasury bonds from a year ago", {"entities": [(19, 22, "currency"), (42, 50, "time_interval"), (23,38, "instrument")]}),
+#     ("Pull up the CAD treasury bonds since today", {"entities": [(12, 15, "currency"), (31, 36, "time_interval"), (16,31, "instrument")]}),
+#     ("Give me USD treasury bonds performance from last week", {"entities": [(8, 11, "currency"), (40, 49, "time_interval"), (12,27, "instrument")]})
+def generate_sentence(currency, instrument, time_interval_phrase):
+    sentence = f"Check the performance of {currency} {instrument} since {time_interval_phrase}"
+    return sentence
 
-# Let's assume 'nlp' is your loaded or blank spaCy language model
+def find_entity_indices(sentence, phrases):
+    return [(sentence.find(phrase), sentence.find(phrase) + len(phrase), label) 
+            for phrase, label in phrases]
+
+def add_labels_to_ner(ner, labels):
+    for label in labels:
+        ner.add_label(label)
+
+def train_ner_model(data, nlp, iterations=100):
+    ner = nlp.get_pipe('ner')
+    optimizer = nlp.begin_training()
+    for itn in range(iterations):
+        random.shuffle(data)
+        losses = {}
+        for text, annotations in data:
+            example = Example.from_dict(nlp.make_doc(text), annotations)
+            nlp.update([example], drop=0.5, losses=losses, sgd=optimizer)
+        print(losses)
+
+# Initialize spacy model and NER component
 nlp = spacy.blank('en')
+if 'ner' not in nlp.pipe_names:
+    ner = nlp.add_pipe('ner', last=True)
 
-# Example text and token-based entities
-token_based_entities = [
-    ('Check the performance of CAD bonds since last month'),
-    ('What was the closing value of USD bonds from the previous day?')
-]
+# Define data structures
+instruments = ["bonds", "treasury bonds"]
+time_intervals = ["today", "yesterday", "last week", "last month", "six months ago", "a year ago"]
+currencies = ["CAD", "USD"]
 
-# Convert token-based entities to character-based entities
-char_based_entities = []
-for text, annotation in token_based_entities:
-    doc = nlp(text)  # Create a Doc object from the text
-    entities = []
-    for start_token, end_token, label in annotation['entities']:
-        start_char = doc[start_token].idx
-        # End char is the start of the last token plus its length
-        end_char = doc[end_token - 1].idx + len(doc[end_token - 1])
-        entities.append((start_char, end_char, label))
-    char_based_entities.append((text, {'entities': entities}))
+# Generate training data
+TRAIN_DATA = []
+entity_labels = set()
 
-# Now 'char_based_entities' contains character-based indices
-print(char_based_entities)
+for currency in currencies:
+    for time_interval in time_intervals:
+        for instrument in instruments:
+            sentence = generate_sentence(currency, instrument, time_interval)
+            phrases = [
+                (currency, 'currency'),
+                (time_interval, 'time_interval'),
+                (instrument, 'instrument')
+            ]
+            entities = find_entity_indices(sentence, phrases)
+            TRAIN_DATA.append((sentence, {"entities": entities}))
+            entity_labels.update([label for _, _, label in entities])
+
+# Add entity labels to NER component
+add_labels_to_ner(ner, entity_labels)
+
+# Train NER model
+with nlp.disable_pipes(*[pipe for pipe in nlp.pipe_names if pipe != 'ner']):
+    train_ner_model(TRAIN_DATA, nlp)
+
+# Save the model to disk (optional)
+# nlp.to_disk(r'C:\Users\Richie\Desktop\models')
+
+# Test the trained model
+test_sentence = "Provide me CAD bonds from yesterday"
+doc = nlp(test_sentence)
+print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
--- text.py
+++ text.py
@@ -4,19 +4,46 @@ from spacy.training import Example
 import random 
 
 TRAIN_DATA = [
-    ("Check the performance of CAD bonds since last month", {"entities": [(25, 28, "currency"), (41, 51, "time_interval")]}),
-    ("What was the closing value of USD bonds from the previous day?", {"entities": [(30, 33, "currency"), (49, 61, "time_interval")]}),
-    ("Get me last week's summary for CAD bonds", {"entities": [(31, 34, "currency"), (7, 18, "time_interval")]}),
-    ("Provide a breakdown of USD bond yields from yesterday", {"entities": [(23, 26, "currency"), (44, 53, "time_interval")]}),
-    # ("I want to review CAD bond rates over the last night", {"entities": [(17, 20, "currency"), (41, 53, "time_interval")]}),
-    # ("USD bond index figures for the last week, please", {"entities": [(0, 3, "currency"), (31, 41, "time_interval")]}),
-    # ("Show the trend for CAD bonds since the fiscal year started", {"entities": [(20, 23, "currency"), (37, 61, "time_interval")]}),
-    # ("Can you give me the historical prices for USD bonds since Q2 began?", {"entities": [(41, 44, "currency"), (51, 61, "time_interval")]}),
-    # ("Pull up the daily returns for CAD bonds for the most recent month", {"entities": [(29, 32, "currency"), (53, 70, "time_interval")]}),
-    # ("Generate a report on USD bonds for the first half of the year", {"entities": [(22, 25, "currency"), (42, 65, "time_interval")]})
-
+    ("Check the performance of CAD bonds since last month", {"entities": [(25, 28, "currency"), (41, 51, "time_interval"), (29,34, "instrument")]}),
+    ("What was the closing value of USD bonds from the previous day?", {"entities": [(30, 33, "currency"), (49, 61, "time_interval"), ( 34,39, "instrument")]}),
+    ("Get me last week's summary for CAD bonds", {"entities": [(31, 34, "currency"), (7, 18, "time_interval"),( 35,40, "instrument")]}),
+    ("Provide a breakdown of USD bonds from yesterday", {"entities": [(23, 26, "currency"), (38, 47, "time_interval"),( 27,32, "instrument") ]}),
+    ("I want to review CAD bonds over the last night", {"entities": [(17, 20, "currency"), (36, 46,  "time_interval"),( 21,26, "instrument")]}),
+    ("USD bond index figures for the last week, please", {"entities": [(0, 3, "currency"), (31, 41, "time_interval"),( 4,14, "instrument")]}),
+    ("Show the trend for CAD bonds from last week", {"entities": [(19, 22, "currency"), (34, 43, "time_interval"),( 23,28, "instrument")]}),
+    ("Pull up the CAD bonds for the last month", {"entities": [(12, 15, "currency"), (30, 40, "time_interval"),( 16,21, "instrument")]}),
+    ("Give me USD bonds from six months ago", {"entities": [(8, 11, "currency"), (23, 37, "time_interval"),( 12,17, "instrument")]})
 ] 
+# instruments = ["bonds", "treasury bonds"]
 
+# time_intervals = [
+#     ("today", "today"),
+#     ("yesterday", "yesterday"),
+#     ("last week", "last week"),
+#     ("last month", "last month"),
+#     ("six months ago", "six months ago"),
+#     ("a year ago", "a year ago")
+# ]
+# currencies = ["CAD", "USD"]
+
+# TRAIN_DATA = []
+
+# for currency in currencies:
+#     for time_interval_phrase, time_interval_label in time_intervals:
+#         for instrument in instruments:
+#             # Construct the sentence
+#             sentence = f"Check the performance of {currency} {instrument} since {time_interval_phrase}"
+#             # Find the start and end indices for entities
+#             currency_idx = sentence.find(currency)
+#             time_interval_idx = sentence.find(time_interval_phrase)
+#             instrument_idx = sentence.find(instrument)
+
+#             # Append to TRAIN_DATA with the correct format
+#             TRAIN_DATA.append((sentence, {"entities": [
+#                 (currency_idx, currency_idx + len(currency), "currency"),
+#                 (time_interval_idx, time_interval_idx + len(time_interval_phrase), "time_interval"),
+#                 (instrument_idx, instrument_idx + len(instrument), "instrument")
+#             ]}))
 
 nlp = spacy.blank('en')
 
@@ -44,8 +71,7 @@ with nlp.disable_pipes(*[pipe for pipe in nlp.pipe_names if pipe != 'ner']):
             nlp.update([example], drop=0.5, losses=losses, sgd=optimizer)
         print(losses)
 
-
-nlp.to_disk(r'C:\Users\Richie\Desktop\models')
-sentence = "Give me EURO bonds from last month"
+#nlp.to_disk(r'C:\Users\Richie\Desktop\models')
+sentence = "Give me CAD treasury bonds from a month ago"
 doc = nlp(sentence)
 print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
